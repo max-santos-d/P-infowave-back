7895f69ac76c0d75247d0eb6bd77d71fca051d3a
Max Santos <max.santosdev@gmail.com>
2024-10-03 12:49:08 -0300

    refactor(api/src/repositories/post.repositories.js): implementing post index route return in descending order

--- api/src/repositories/post.repositorie.js
+++ api/src/repositories/post.repositorie.js
@@ -1,7 +1,7 @@
 import Post from '../models/Post.js';
 
 const store = (body) => Post.create(body);
-const index = () => Post.find().populate('user');
+const index = () => Post.find().populate('user').sort({ updated_at: -1 });
 const show = (id) => Post.findById({ _id: id }).populate('user');
 const update = (id, body) => Post.findOneAndUpdate({ _id: id }, body, { new: true });
 const deleted = (id) => Post.findOneAndDelete({ _id: id });
