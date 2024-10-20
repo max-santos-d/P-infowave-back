875cb2c18a0107c25d34bb3a016f8afe9f1c24d9
Max Santos <max.santosdev@gmail.com>
2024-10-20 17:02:35 -0300

    refactor: question index route return to include additional user information from comments

--- src/repositories/question.repositorie.js
+++ src/repositories/question.repositorie.js
@@ -1,20 +1,44 @@
 import Question from '../models/Question.js';
 
 const store = (text, user) => Question.create({ text, user });
-const index = () => Question.find().sort({ _id: -1 }).populate('user');
-const show = (id) => Question.findById({ _id: id }).populate('user');
-const update = (id, body) => Question.findOneAndUpdate({ _id: id }, body, { new: true });
-const deleted = (id) => Question.findOneAndDelete({ _id: id });
+
+const index = () =>
+  Question.find()
+    .populate('user', 'username status')
+    .populate({
+      path: 'comments.user',
+      select: 'username status',
+    })
+    .sort({ updated_at: -1 })
+    .exec();
+
+const show = (id) =>
+  Question.findById({ _id: id })
+    .populate('user', 'username status')
+    .populate({
+      path: 'comments.user',
+      select: 'username status',
+    })
+    .sort({ updated_at: -1 })
+    .exec();
+
+const update = (id, body) => Question.findOneAndUpdate({ _id: id }, body, { new: true }).exec();
+
+const deleted = (id) => Question.findOneAndDelete({ _id: id }).exec();
 
 const search = (searchText) =>
   Question.find({
-    $or: [
-      { title: { $regex: searchText, $options: 'i' } }, // 'i' para case insensitive
-      { text: { $regex: searchText, $options: 'i' } },
-    ],
-  });
+    $or: [{ title: { $regex: searchText, $options: 'i' } }, { text: { $regex: searchText, $options: 'i' } }],
+  }).exec();
 
-const searchByUser = (userId) => Question.find({ user: userId }).populate('user');
+const searchByUser = (userId) =>
+  Question.find({ user: userId })
+    .populate('user', 'username status')
+    .populate({
+      path: 'comments.user',
+      select: 'username status',
+    })
+    .exec();
 
 export default {
   store,
